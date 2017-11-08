cc6de8c07784d9a4f5855c998b64071c117b2e44
Doron Guttman <dguttman@jacada.com>
2017-11-08 10:04:19 -0500

    Solution: + Common.Utils project

---
+++ source/Common/ConsoleTools.Common.Utils/Diagnostics/Debugger.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace ConsoleTools.Common.Utils.Diagnostics
+{
+    public static class Debugger
+    {
+        [Conditional("DEBUG")]
+        public static void BreakOnDebug(bool condition = true)
+        {
+            if (condition) System.Diagnostics.Debugger.Break();
+        }
+
+        public static void Break(bool condition = true)
+        {
+            if (condition) System.Diagnostics.Debugger.Break();
+        }
+    }
+}
